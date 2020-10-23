dd9d14bdc9cab4bf23b367bf0c19d6933ea5f982
WN-628 <WN-628@noreply.github.com>
2020-10-23 09:23:25 +0800

    find oscillation period failed

--- Oscillation/Oscillation.py
+++ Oscillation/Oscillation.py
@@ -179,7 +179,73 @@ def next_theta(theta, Omega):
 for i in range(1, steps):
     O_arr[i] = next_Omega(Theta_arr[i - 1], O_arr[i - 1])
     Theta_arr[i] = next_theta(Theta_arr[i - 1], O_arr[i])
+# plt.plot(t_arr, Theta_arr)
+# # plt.plot(t_arr, O_arr)
+# plt.show()
+
+# %%
+def findzeros(a_arr, start):
+    for i in range(start, len(a_arr)):
+        if (a_arr[i] > 0 and a_arr[i + 1] < 0) or (a_arr[i] < 0 and a_arr[i + 1] > 0):
+            return i + 1
+
+def findperiod(arr):
+    t1index = findzeros(arr, 0)
+    t2index = findzeros(arr, t1index)
+    return [t1index, t2index]
+
+index = findperiod(Theta_arr)
+period = (((t_arr[index[1]] + t_arr[index[1] - 1])/2) - ((t_arr[index[0]] + t_arr[index[0] - 1])/2))*2
+print("The period of this motion is ", period)
 plt.plot(t_arr, Theta_arr)
-# plt.plot(t_arr, O_arr)
 plt.show()
+
+#%%
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+m = 1
+L = 10
+g = 9.8
+T_max = 50  #maximum time span
+stepsT = 1000
+dt = T_max / stepsT  #Discretize time
+theta_min = 1
+theta_max = 89
+steps = 20
+theta0_arr = np.linspace(theta_min, theta_max, steps)
+period_arr = np.zeros(steps)
+
+def findzeros(a_arr, start):
+    for i in range(start + 1, len(a_arr)):
+        if (a_arr[i - 1] > 0 and a_arr[i] < 0) or (a_arr[i - 1] < 0 and a_arr[i] > 0):
+            return i
+
+def findperiod(arr):
+    t1index = findzeros(arr, 0)
+    t2index = findzeros(arr, t1index)
+    return [t1index, t2index]
+
+for i in theta0_arr:
+    t_arr = np.linspace(0, T_max, stepsT)
+    Theta_arr = np.zeros(stepsT)
+    Theta_arr[0] = i*2*np.pi/360
+    O_arr = np.zeros(steps)
+    O_arr[0] = 0
+    def next_Omega(theta, Omega):
+        return Omega - g/L*np.sin(theta)*dt 
+
+    def next_theta(theta, Omega):
+        return theta + Omega*dt
+
+    for i in range(1, steps):
+        O_arr[i] = next_Omega(Theta_arr[i - 1], O_arr[i - 1])
+        Theta_arr[i] = next_theta(Theta_arr[i - 1], O_arr[i])
+    index = findperiod(Theta_arr)
+    period_arr[i] = (((t_arr[index[1]] + t_arr[index[1] - 1])/2) - ((t_arr[index[0]] + t_arr[index[0] - 1])/2))*2
+
+plt.plot(theta0_arr, period_arr)
+plt.show()
+    
 # %%
