bccac6a73901d00c2ee24be52bdac5e836f8102f
WN-628 <72026303+WN-628@users.noreply.github.com>
2020-11-23 09:23:02 +0800

    Update Pendulum.py

--- Graphs/Pendulum.py
+++ Graphs/Pendulum.py
@@ -8,14 +8,18 @@ from vpython import *
 bob = sphere(pos = vector(5, 2, 0), radius = 0.5)
 pivot = vector(0, 20, 0)
 Ceiling = box(pos = pivot, size = vector(10, 0.5, 10), color = color.green)
-rod = helix(pos = pivot, axis = bob.pos-pivot, radius = 0.5)
+rod = helix(pos = pivot, axis = bob.pos-pivot, radius = 0.5, coils = 10)
 
 # Constant
+m = 1
 g = 9.8 
 L = mag(bob.pos - pivot) #length of pendulum
 cs = (pivot.y - bob.pos.y)/L #Calculation of cos(theta)
 theta = acos(cs) #angle wih vertical direction
 angvel = 0.0 #angular velocity
+k = 20 #Spring constant
+v_x = 0
+v_y = 0
 
 # Initialize the time
 tmin = 0
@@ -26,10 +30,15 @@ dt = (tmax - tmin)/step
 t = 0
 
 while(t < tmax):
+    Fel = k*(((L*sin(theta))**2 + (pivot.y - L*cos(theta))**2) - L) #Elastic force of the spring
+    acc_x = Fel*sin(theta)/m
+    v_x += acc_x*dt
+    acc_y = (- m*g + Fel*cos(theta))/m
+    v_y += acc_y*dt
     rate(100) #maximum 100 calculations per second
     acc = -g/L*sin(theta) #updatin of auglar acceleration
     theta = theta + angvel*dt #updating of angular position
     angvel = angvel + acc*dt #updating of angular velocity
-    bob.pos = vector(L*sin(theta), pivot.y - L*cos(theta), 0) #cal. postition
+    bob.pos = vector(L*sin(theta) + v_x*dt, pivot.y - L*cos(theta) + v_y*dt, 0) #cal. postition
     rod.axis = bob.pos - rod.pos #updating other end of rod of pendulum 
     t = t + dt # updating time
