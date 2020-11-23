dd1337c630173718dd7d72b57a7e3c85f67d0096
WN-628 <72026303+WN-628@users.noreply.github.com>
2020-11-23 08:34:48 +0800

    Updare Pendulum

--- Graphs/Pendulum.py
+++ Graphs/Pendulum.py
@@ -8,7 +8,7 @@ from vpython import *
 bob = sphere(pos = vector(5, 2, 0), radius = 0.5)
 pivot = vector(0, 20, 0)
 Ceiling = box(pos = pivot, size = vector(10, 0.5, 10), color = color.green)
-rod = helix(pos = pivot, axis = bob.pos-pivot, radius = 0.1, color = color.red)
+rod = helix(pos = pivot, axis = bob.pos-pivot, radius = 0.5)
 
 # Constant
 g = 9.8 
@@ -23,6 +23,13 @@ tmax = 100
 step = 10000
 dt = (tmax - tmin)/step
 
+t = 0
+
 while(t < tmax):
     rate(100) #maximum 100 calculations per second
-    acc
+    acc = -g/L*sin(theta) #updatin of auglar acceleration
+    theta = theta + angvel*dt #updating of angular position
+    angvel = angvel + acc*dt #updating of angular velocity
+    bob.pos = vector(L*sin(theta), pivot.y - L*cos(theta), 0) #cal. postition
+    rod.axis = bob.pos - rod.pos #updating other end of rod of pendulum 
+    t = t + dt # updating time
